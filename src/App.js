353cbc3b69f1d00bb75ae0a44f3f6b96ae4e3a04
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-25 14:07:51 +0000

    data from API loads in container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import CountryContainer from './Containers/CountryContainer';
 
 function App() {
   return (
     <>
+      <CountryContainer />
     </>
   );
 }
--- src/Containers/CountryContainer.js
+++ src/Containers/CountryContainer.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CountryContainer = () => {
     
     const [allCountries, setAllCountries] = useState([]);
     const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const loadCountries = async () => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const jsonData = await response.json();
+        setAllCountries(jsonData);
+    }
+
+    useEffect(
+        () => {
+            loadCountries();
+        }, []
+    );
     
     return (
         <>
+            {JSON.stringify(allCountries)}
         </>
     );
 }
