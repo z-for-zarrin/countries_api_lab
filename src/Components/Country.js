82ae1341482878eef08c5505425aa9a75d5067bb
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-25 18:08:32 +0000

    Each country in list expands to give more information

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -5,16 +5,23 @@ const Country = ({ country, toggleLists }) => {
     }
 
     return (
-        <li>
-            <label htmlFor="visited">{country.flag} {country.name.common}</label>
-            <input
-                type="button"
-                name="visited"
-                className="toggleButton"
-                value="✓"
-                onClick={handleCheckbox}
-            />
-        </li>
+        <>
+            <details>
+                <summary>
+                    <label htmlFor="visited">{country.flag} {country.name.common}</label>
+                    <input
+                        type="button"
+                        name="visited"
+                        className="toggleButton"
+                        value="✓"
+                        onClick={handleCheckbox}
+                    />
+                </summary>
+                <p>Capital(s): {country.capital ? country.capital.join(", ") : "None"}</p>
+                <p>Region: {country.region}</p>
+                <p>Languages: {country.languages ? Object.values(country.languages).join(", ") : "N/A"}</p>
+            </details>
+        </>
     );
 }
  
